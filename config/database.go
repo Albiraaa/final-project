8051dfc9eb3b49ca670b757849ab133b2c51644b
Albiraaa <aldobintangrhamadhan_23051450223@radenfatah.ac.id>
2025-12-13 07:30:11 +0700

    s

--- config/database.go
+++ config/database.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
